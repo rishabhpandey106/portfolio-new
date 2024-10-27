0f5afe4ef52ea7d7272f5c0aa8c250cb2ed66cde
rishabhpandey106 <rishabhpandey230@gmail.com>
2024-10-28 00:09:37 +0530

    fixed some bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
